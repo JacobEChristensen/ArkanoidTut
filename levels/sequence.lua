ffb00e48559392964c0c81a30daaf177023b8f1c
JacobEChristensen <jacob.engbjerg.christensen@gmail.com>
2020-11-25 13:24:12 +0100

    added more levels

--- levels/sequence.lua
+++ levels/sequence.lua
@@ -1,15 +1,38 @@
 return {
 	"test_all",
 	"intro",
+	"pillars",
+	"smiley",
 	"checkers",
 	"ying_yang",
 	"stair",
 	"lines",
 	"protected_c",
-	"towers"
+	"protected",
+	"angry_smiley",
+	"butterfly"
 }
 
 -- Level list outline
-
-
--- BOSS LEVEL
+--[[	
+	1. intro
+	2. pillars
+	3. Smiley
+	4. ying_yang
+	5. checkers
+	6. stair
+	7. lines
+	8.
+	9.
+	10. protected_c
+	11.
+	12. protected
+	13. Angry smiley
+	14.
+	15. butterfly
+	16
+	17.
+	18.
+	19.
+	20. BOSS
+--]]
